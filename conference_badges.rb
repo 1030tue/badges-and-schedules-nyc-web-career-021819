132403d500d67793cc488be6dbbabb74ceec8209
Kate <katekim5809@gmail.com>
2019-02-04 13:57:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attandees)
+	arr = []
+	i = 0
+	while i < attandees.size
+puts  arr << "Hello, my name is " + attandees[i]
+i+=0 
+end
+
+end
+  
+
+
+def assign_rooms
+  
+end
+
+def printer
+  
+end
